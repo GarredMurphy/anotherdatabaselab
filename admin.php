5305f71e6d0f327a00d3ff7323e904fabbff43b2
Garred Buehl Murphy <gmurphy@csumb.edu>
2017-11-05 22:25:48 +0000

    small glitch fixed, definitely final this time

--- admin.php
+++ admin.php
@@ -31,7 +31,7 @@ function userList()
         <script>
             
             function confirmDelete(){
-                confirm("Are you sure you want to delete this user?")
+                return confirm("Are you sure you want to delete this user?")
             }
         </script>
     </head>
